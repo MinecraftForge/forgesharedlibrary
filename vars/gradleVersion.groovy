6963dcc80920b6c123d8e3e36fb918f998fb2c3c
cpw <cpw+github@weeksfamily.ca>
2021-04-24 18:24:47 -0400

    try passing in env

--- vars/gradleVersion.groovy
+++ vars/gradleVersion.groovy
@@ -1,6 +1,6 @@
-def call(String properties = "properties", env, String env_property) {
+def call(Script script, String properties = "properties", String env_property = "MY_VERSION") {
     def data = sh(returnStdout: true, script: "./gradlew ${properties} -q").trim().toString()
     def props = [:]
     data.split('\n').each { line -> line.tokenize(':').with { if (it.size() == 2) props[it[0].trim()] = it[1].trim() }}
-    env[env_property] = props.get('version', 'Missing')
+    script.env[env_property] = props.get('version', 'Missing')
 }
