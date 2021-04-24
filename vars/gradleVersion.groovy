60b29a3d319bbe2fe04d9b59d23837b0fc5811d4
cpw <cpw+github@weeksfamily.ca>
2021-04-24 17:46:21 -0400

    toString?

--- vars/gradleVersion.groovy
+++ vars/gradleVersion.groovy
@@ -1,5 +1,5 @@
 def call(String properties = "properties") {
-    data = sh(returnStdout: true, script: './gradlew ${properties} -q').trim()
+    data = sh(returnStdout: true, script: './gradlew ${properties} -q').trim().toString()
     props = [:]
     data.splitEachLine { props[it[0]] = it[1]}
     return props.get("version", "Missing")
