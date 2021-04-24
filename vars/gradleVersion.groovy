8aa962e0ac77c0d376c1d1e2f9c1c412fd215560
cpw <cpw+github@weeksfamily.ca>
2021-04-24 17:56:32 -0400

    try better?

--- vars/gradleVersion.groovy
+++ vars/gradleVersion.groovy
@@ -1,7 +1,7 @@
 def call(String properties = "properties") {
     def data = sh(returnStdout: true, script: "./gradlew ${properties} -q").trim().toString()
     def props = [:]
-    data.split('\n').each { it.split(':') .each { props[it[0]] = it[1]}}
+    data.split('\n').each { line -> line.split(':').each { props[it[0]] = it[1]}}
     echo props
     return props.get("version", "Missing")
 }
