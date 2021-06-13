d9a79850d75d8c070680997cb51406666745beb0
cpw <cpw+github@weeksfamily.ca>
2021-06-13 14:43:17 -0400

    allow accessing multiple properties from the gradleversion task

--- vars/gradleVersion.groovy
+++ vars/gradleVersion.groovy
@@ -1,6 +1,6 @@
-def call(Script script, String properties = "properties", String env_property = "MY_VERSION") {
+def call(Script script, String properties = "properties", Map variables = ["version":"MYVERSION", "group":"MYGROUP", "name": "MYARTIFACT"]) {
     def data = sh(returnStdout: true, script: "./gradlew ${properties} -q").trim().toString()
     def props = [:]
     data.split('\n').each { line -> line.tokenize(':').with { if (it.size() == 2) props[it[0].trim()] = it[1].trim() }}
-    script.env[env_property] = props.get('version', 'Missing')
+    variables {(k,v) -> script.env[v] = props.get(k, 'Missing')}
 }
